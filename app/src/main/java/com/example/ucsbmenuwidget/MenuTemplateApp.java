7ff8ef66436e12c44dbcf97c819b3825ae750023
Jonathan <jxu460@gmail.com>
2019-07-31 23:21:06 -0700

    Changing scraping method to take advantage of selector syntax in jsoup.

--- app/src/main/java/com/example/ucsbmenuwidget/MenuTemplateApp.java
+++ app/src/main/java/com/example/ucsbmenuwidget/MenuTemplateApp.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
+
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -16,6 +17,7 @@ import java.io.IOException;
 public class MenuTemplateApp extends Activity {
     TextView menuView;
     String menuText = "";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,13 +36,13 @@ public class MenuTemplateApp extends Activity {
     }
 
     //change method to run in async task
-    private class menuGetter extends AsyncTask<Void, Void, Void>{
+    private class menuGetter extends AsyncTask<Void, Void, Void> {
         private void getMenu() {
             try {
                 //Connect to the website
                 Document document = Jsoup.connect("https://appl.housing.ucsb.edu/menu/day/").get();
                 //change so not hard coded for carrillo
-                String diningCommon = ((TextView)findViewById(R.id.my_textview)).getText().toString();
+                String diningCommon = ((TextView) findViewById(R.id.my_textview)).getText().toString();
                 //System.out.println(diningCommon);
                 switch (diningCommon) {
                     case "Carrillo":
@@ -59,9 +61,23 @@ public class MenuTemplateApp extends Activity {
                         diningCommon = "Invalid";
                         break;
                 }
-                Elements menuItems = document.select("#" + diningCommon + " > *");
-                for (Element e : menuItems) {
+                /* this line finds the menu for a dining common using the id tag (#), then
+                * it goes through all its children, and looks through the children's children
+                * for elements with class panel-body. This creates a list of menus based on meal time*/
+                Elements meals = document.select("#" + diningCommon + " > * > div.panel-body");
+                Elements mealTitles = document.select("#" + diningCommon + " > * > div.panel-heading");
+                for (Element e : mealTitles) {
                     //change so that you individually parse each child elements' contents to properly add new lines.
+                    /*
+                    Elements mealItems = e.select("*");
+                    for (Element meal : mealItems) {
+                        if (meal.hasClass("panel-heading")) {
+                            menuText += meal.text();
+                            menuText += "\n";
+                        } else if (meal.hasClass("panel-body")) {
+                            Elements
+                        }
+                    }*/
                     menuText += e.text();
                     menuText += "\n";
                 }
